1a767ca5ae59e81a7009e9de9306dbefd5c3f312
tien <tientrinh257@bitbucket.org>
2018-10-18 18:37:44 +0700

    Updates

---
+++ precache-manifest.52733fb97fa939d975015bacfc574e0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myfirstapp/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "60a68c37ad126d3b5086",
+    "url": "/myfirstapp/static/js/runtime~main.60a68c37.js"
+  },
+  {
+    "revision": "3c35036e7192d73e350b",
+    "url": "/myfirstapp/static/js/main.3c35036e.chunk.js"
+  },
+  {
+    "revision": "855276873adeae1ea0f1",
+    "url": "/myfirstapp/static/js/1.85527687.chunk.js"
+  },
+  {
+    "revision": "3c35036e7192d73e350b",
+    "url": "/myfirstapp/static/css/main.bac89c94.chunk.css"
+  },
+  {
+    "revision": "3ec5dae6f0705c5785f5a80aac5a06da",
+    "url": "/myfirstapp/index.html"
+  }
+];
